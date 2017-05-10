d407648ce7468afc6de23f38f462082110e9a52d
Anton <zabidok@gmail.com>
2017-05-10 11:58:09 +0300

    fix

--- seznam/my.js
+++ seznam/my.js
@@ -1,22 +1,7 @@
-// $(function(){
-//     $('.f a').click(function(){
-//         var text=$(this).text().toLowerCase();
-//         $('.a li').hide();
-//         var li=$('.a li').filter(function(){
-//             return $(this).attr('class').indexOf(text)>-1;
-//         });
-//         li.show();
-//     });
-//
-// });
-
-
 function select(cat, elem) {
     var btns = document.getElementsByClassName('filter');
     var elems_all = document.getElementsByClassName('filter_item');
     var elems_selected = document.getElementsByClassName(cat);
-    // elems_all.style.display='none';
-    // elems_all.style.style.visibility='visible';
     [].forEach.call(btns, function (el) {
         el.classList.remove('active');
     });
@@ -29,9 +14,5 @@ function select(cat, elem) {
         el.style.display = 'inline-block';
     });
     elem.className += ' active';
-
-    console.log('select');
-
-
 }
 
